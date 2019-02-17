cc1867aaf6a861ade6b76fa8700b1e98278c7f22
Hannah Rose <hannaesthetic.r@gmail.com>
2019-02-17 13:22:02 +0000

    Added part 1

---
+++ Part 1/Assets/Scripts/Brush.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brush : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+        gameObject.transform.position = mousePos;
+        spriteRenderer.enabled = Input.GetMouseButton(0);
+    }
+}
---
+++ Part 1/Assets/Scripts/DrawingCamera.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawingCamera : MonoBehaviour
+{
+
+    public Camera drawingCamera;
+    public RawImage overlayImage;
+    
+    void Start()
+    {
+        drawingCamera.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        overlayImage.texture = drawingCamera.targetTexture;
+    }
+}
